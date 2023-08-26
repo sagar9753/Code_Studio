7836a2363113b45ffd3b363a3a8ef35dad3791fd
Sagar Rajput <sagar.rajput9753@gmail.com>
2023-08-26 13:12:10 +0530

    Create Set The Rightmost Unset Bit.java

---
+++ Set The Rightmost Unset Bit.java
@@ -0,0 +1,21 @@
+Ques Link - https://www.codingninjas.com/studio/problems/set-the-rightmost-unset-bit_8160456?utm_source=striver&utm_medium=website&utm_campaign=a_zcoursetuf
+
+--------------------------------------------------- Solution -------------------------------------------------------
+
+public class Solution {
+    public static int setBits(int N){
+        // Write your code here.
+        int num = N ;
+        int count = 0;
+        while(num % 2 != 0){
+            num = num>>1;
+            count++;
+        }
+        if(num >> 1 % 2 != 0){
+            num |= 1;
+            num = num<<count;
+            return N | num;
+        } 
+        return N;
+    }
+}
